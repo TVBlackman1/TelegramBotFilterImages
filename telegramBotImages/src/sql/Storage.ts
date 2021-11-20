b92c48069e9c98caffa67c3ef0f13578d386341e
tvblackman1 <vbif.misha2012@yandex.ru>
2021-11-20 21:25:23 +0300

    ...

--- telegramBotImages/src/sql/Storage.ts
+++
@@ -1,9 +0,0 @@
-import {IStorage, IUserStorageManager} from "../entities.global/db.entities";
-
-export class Storage implements IStorage {
-    public readonly user: IUserStorageManager;
-    constructor(_user: IUserStorageManager,) {
-        this.user = _user
-    }
-}
-// const storage = Storage()
